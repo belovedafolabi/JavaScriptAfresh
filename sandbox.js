4b04fb8ba0ca40bc0f980bb654be48fddd86bc59
afolabi <belovedafolabi@gmail.com>
2024-08-12 13:14:14 +0100

    Ninja To Do Project start

--- sandbox.js
+++ sandbox.js
@@ -1,38 +1,21 @@
-// Form Element
-const form = document.getElementById("ninjaForm");
-// Score Display Element
-const scoreContent = document.getElementById("scoreContent");
-// Score Percentage
-const scorePercentage = document.getElementById("scorePercentage");
+// Add Todo Element
+const todoInput = document.querySelector('#todoAdd');
+// To DO List Element
+const todoDivList = document.querySelector('#toDoDivList');
 
+// Add Listener to for Key press action
+todoInput.addEventListener('keydown', (event)=>{
 
-form.addEventListener('submit', e =>{
-    e.preventDefault();
-    // Mark quiz function from quiz.js
-    const score = markQuiz(questionAnswers);
-
-    // Percentage based on user score
-    const scorePercent = ( score / questionAnswers.length) * 100;
-    let i = 0;
-    // Timer to animate score by increasing from zero to actual score
-    const timer = setInterval(()=>{
-        i++;
-        scorePercentage.textContent = ` ${i}% `;
-        if(i === scorePercent){
-            clearInterval(timer);
-        }
-    }, 50);
-
-    // Display the score percentage after quiz is submitted
-    scoreContent.classList.remove('hidden');
-    
-    // Scroll back to top smoothly
-    function smoothScrollToTop() {
-        const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-        if (currentScroll > 0) {
-            window.requestAnimationFrame(smoothScrollToTop);
-            window.scrollTo(0, currentScroll - currentScroll / 11);
+    // Check that it's the enter key that was pressed.
+    if(event.key === 'Enter'){
+        //Ensure To Do is filled before submission
+        if(todoInput.value){
+            todoDivList.append('')
+            // Empty the input field
+            todoInput.value = "";
+        }else{
+            alert('bad');
         }
     }
-    smoothScrollToTop();
+
 });
